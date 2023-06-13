dfa553f970736d0cfd4a5b4e4c2f837b5f34a398
Anna Gabain <anna.gabain@outlook.com>
2023-06-13 11:21:43 +0000

    Set up initial header and logo

--- mom-network/src/App.js
+++ mom-network/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import logo from "./mom_network_logo.png";
+import "./App.css";
 
 function App() {
   return (
@@ -7,17 +8,28 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Hello Mom Network! */}
+          {/* Edit <code>src/App.js</code> and save to reload. */}
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <a className="App-link">Home</a>
+        <a className="App-link">Feed</a>
+        <a className="App-link">Groups</a>
+        <a className="App-link">My network</a>
       </header>
+      <br/>
+      <br/>
+      <br/>
+      <h1>
+          Hello Mom Network!
+        </h1>
     </div>
   );
 }
