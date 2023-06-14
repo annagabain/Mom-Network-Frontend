030a1ec28f126383e4007509d54bfb942cf10c6a
Anna Gabain <anna.gabain@outlook.com>
2023-06-14 14:21:10 +0000

    Comment out axios for now

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,8 @@
+// import axios from "axios";
+
+// axios.defaults.baseURL = 'https://mom-network-backend.herokuapp.com'
+// axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+// axios.defaults.withCredentials = true
+
+// export const axiosReq = axios.create()
+// export const axiosRes = axios.create()
