c5e66f5d67141326dd9a2448e36b0255bfddd0d4
Anna Gabain <anna.gabain@outlook.com>
2023-07-13 12:52:14 +0000

    Make a basic Navigation Bar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import TestApiComponent from "./components/TestApiComponent";
 // import ProfilesApiComponent from "./components/ProfilesApiComponent";
 import PostsApiComponent from "./components/PostsApiComponent";
-import Button from 'react-bootstrap/Button';
+import NavigationBar from './components/NavigationBar';
 
 
 function App() {
@@ -12,19 +12,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-        <Button variant="primary">Primary</Button>
-        <a className="App-link">Home</a>
-        <a className="App-link">Feed</a>
-        <a className="App-link">Groups</a>
-        <a className="App-link">My network</a>
+        <NavigationBar/>
       </header>
       <br />
       <br />
