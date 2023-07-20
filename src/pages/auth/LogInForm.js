ff325b7bbdc9ddf6ef6570094677fe945cda57d8
Anna Gabain <anna.gabain@outlook.com>
2023-07-20 13:22:28 +0000

    Redirect forms after submission

--- src/pages/auth/LogInForm.js
+++ src/pages/auth/LogInForm.js
@@ -1,14 +1,10 @@
 import React, { useState } from "react";
 import axios from "axios";
-
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
 import Alert from "react-bootstrap/Alert";
+import { useHistory } from "react-router-dom";
 
-// import { useHistory } from "react-router-dom";
 
 function LogInForm() {
   const [logInData, setLogInData] = useState({
@@ -18,7 +14,7 @@ function LogInForm() {
   const { username, password } = logInData;
   const [errors, setErrors] = useState({});
 
-  // const history = useHistory();
+  const history = useHistory();
 
   const handleChange = (event) => {
     setLogInData({
@@ -31,57 +27,53 @@ function LogInForm() {
     event.preventDefault();
     try {
       await axios.post("/dj-rest-auth/login/", logInData);
-      // history.push("/");
+      history.push("/feed");
     } catch (err) {
       setErrors(err.response?.data);
     }
   };
 
   return (
-    <Row>
-      <Col className="my-auto p-0 p-md-2" md={6}>
-        <Container className={` p-4 `}>
-          <h3>Login</h3>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="formBasicUsername">
-              <Form.Label className="d-none">Username</Form.Label>
-              <Form.Control
-                type="text"
-                name="username"
-                placeholder="Enter username"
-                value={username}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            {errors.username?.map((message, idx) => (
-              <Alert key={idx} variant="warning">
-                {message}
-              </Alert>
-            ))}
+    <>
+      <br />
+      <h3>Login</h3>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicUsername">
+          <Form.Label className="d-none">Username</Form.Label>
+          <Form.Control
+            type="text"
+            name="username"
+            placeholder="Enter username"
+            value={username}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        {errors.username?.map((message, idx) => (
+          <Alert key={idx} variant="warning">
+            {message}
+          </Alert>
+        ))}
 
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label className="d-none">Password</Form.Label>
-              <Form.Control
-                type="password"
-                name="password"
-                placeholder="Password"
-                value={password}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            {errors.password?.map((message, idx) => (
-              <Alert key={idx} variant="warning">
-                {message}
-              </Alert>
-            ))}
-            <Button variant="primary" type="submit">
-              Login
-            </Button>
-          </Form>
-        </Container>
-      </Col>
-      <Col md={6} className={`my-auto d-none d-md-block p-2`}></Col>
-    </Row>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label className="d-none">Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        {errors.password?.map((message, idx) => (
+          <Alert key={idx} variant="warning">
+            {message}
+          </Alert>
+        ))}
+        <Button className="button" variant="primary" type="submit">
+          Login
+        </Button>
+      </Form>
+    </>
   );
 }
 
--- src/pages/auth/RegisterForm.js
+++ src/pages/auth/RegisterForm.js
@@ -1,18 +1,8 @@
 import React, { useState } from "react";
-// import { Link, useHistory } from "react-router-dom";
-// import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
-import {
-  Form,
-  Button,
-  Col,
-  Row,
-  Container,
-  Alert,
-} from "react-bootstrap";
+import { Form, Button, Container, Alert } from "react-bootstrap";
 import axios from "axios";
-
-
 
 const RegisterForm = () => {
   const [registerData, setRegisterData] = useState({
@@ -24,7 +14,7 @@ const RegisterForm = () => {
 
   const [errors, setErrors] = useState({});
 
-  // const history = useHistory();
+  const history = useHistory();
 
   const handleChange = (event) => {
     setRegisterData({
@@ -37,92 +27,90 @@ const RegisterForm = () => {
     event.preventDefault();
     try {
       await axios.post("/dj-rest-auth/registration/", registerData);
-      // history.push("/signin");
+      history.push("/login");
     } catch (err) {
       setErrors(err.response?.data);
     }
   };
 
-  
   return (
-    <Row>
-      <Col className="my-auto py-2 p-md-2" md={6}>
-        <Container className="p-4">
-          <h3>Register</h3>
+    <>
+      <br />
+      <br />
+      <br />
 
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="username">
-              <Form.Label className="d-none">username</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Username"
-                name="username"
-                value={username}
-                onChange={handleChange}
-              />
-              {errors && errors.username?.map((message, idx) => (
-                <Alert variant="warning" key={idx}>
-                  {message}
-                </Alert>
-              ))}
-            </Form.Group>
+      <h3>Register</h3>
 
-            <Form.Group controlId="password1">
-              <Form.Label className="d-none">Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                name="password1"
-                value={password1}
-                onChange={handleChange}
-              />
-              {errors && errors.password1?.map((message, idx) => (
-                <Alert key={idx} variant="warning">
-                  {message}
-                </Alert>
-              ))}
-            </Form.Group>
-
-            <Form.Group controlId="password2">
-              <Form.Label className="d-none">Confirm password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Confirm password"
-                name="password2"
-                value={password2}
-                onChange={handleChange}
-              />
-              {errors && errors.password2?.map((message, idx) => (
-                <Alert key={idx} variant="warning">
-                  {message}
-                </Alert>
-              ))}
-            </Form.Group>
-
-            <Button type="submit" className="button">Register</Button>
-
-            {errors && errors.non_field_errors?.map((message, idx) => (
-              <Alert key={idx} variant="warning" className="mt-3">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="username">
+          <Form.Label className="d-none">username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Username"
+            name="username"
+            value={username}
+            onChange={handleChange}
+          />
+          {errors &&
+            errors.username?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
                 {message}
               </Alert>
             ))}
-          </Form>
-        </Container>
+        </Form.Group>
 
-        <Container className="mt-3">
-          {/* <Link to="/signin">
-            Already have an account? <span>Sign in</span>
-          </Link> */}
-        </Container>
-      </Col>
+        <Form.Group className="mb-3" controlId="password1">
+          <Form.Label className="d-none">Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password1"
+            value={password1}
+            onChange={handleChange}
+          />
+          {errors &&
+            errors.password1?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
+        </Form.Group>
 
-      <Col md={6} className="my-auto d-none d-md-block p-2">
-        {/* <Image
-          className=""
-          src="https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero2.jpg"
-        /> */}
-      </Col>
-    </Row>
+        <Form.Group className="mb-3" controlId="password2">
+          <Form.Label className="d-none">Confirm password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Confirm password"
+            name="password2"
+            value={password2}
+            onChange={handleChange}
+          />
+          {errors &&
+            errors.password2?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
+        </Form.Group>
+
+        <Button type="submit" className="button">
+          Register
+        </Button>
+
+        {errors &&
+          errors.non_field_errors?.map((message, idx) => (
+            <Alert key={idx} variant="warning" className="mt-3">
+              {message}
+            </Alert>
+          ))}
+      </Form>
+
+      <Container className="mt-3">
+        <Link to="/login">
+          Already have an account? <span>Log in</span>
+        </Link>{" "}
+      </Container>
+    </>
   );
 };
 
