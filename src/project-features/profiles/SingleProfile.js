85c16c7901ff267bbaf459ca9a56163de8c1adf9
Anna Gabain <anna.gabain@outlook.com>
2023-09-11 15:38:22 +0000

    Style SingleProfile and message form placeholder

--- src/project-features/profiles/SingleProfile.js
+++ src/project-features/profiles/SingleProfile.js
@@ -3,6 +3,7 @@ import { useParams, useHistory } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import { Col, Form, Row } from "react-bootstrap";
 
 function SingleProfile() {
   const { profileId } = useParams();
@@ -44,32 +45,62 @@ function SingleProfile() {
         {isLoading ? (
           <p>Loading...</p>
         ) : (
-          <Card className="mb-3" style={{ width: "66%" }}>
-            <Card.Body>
-              <h4>{profile.owner}</h4>
-              {profile.image && (
-                <img
-                  src={profile.image}
-                  alt={`Profile of ${profile.owner}`}
-                  style={{
-                    borderRadius: "50%",
-                    width: "180px",
-                    height: "180px",
-                  }}
-                />
-              )}
-              <p>Name: {profile.name}</p>
-              <p>Member since: {profile.created_at}</p>
-              <p>Bio: {profile.content}</p>
-            </Card.Body>
-          </Card>
+          <Row>
+            <Col xs={12} md={8}>
+              <Card className="mb-3" style={{ backgroundColor: "pink" }}>
+                <Card.Body>
+                  <Row>
+                    <Col xs={6} md={4}>
+                      {profile.image && (
+                        <img
+                          src={profile.image}
+                          alt={`Profile of ${profile.owner}`}
+                          style={{
+                            borderRadius: "50%",
+                            width: "180px",
+                            height: "180px",
+                          }}
+                        />
+                      )}
+                    </Col>
+                    <Col xs={12} md={8}>
+                      <h4>{profile.owner}</h4>
+
+                      {/* <p>Name: {profile.name}</p> */}
+                      <p>Member since: {profile.created_at}</p>
+                      <p>Bio: {profile.content}</p>
+                    </Col>
+                  </Row>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col xs={12} md={4}>
+              <Card style={{ backgroundColor: "lightgrey" }}>
+                <Card.Body>
+                  {/* <Form onSubmit={handleSubmit}> */}
+                  <Form>
+                    <Form.Group className="mb-3">
+                      <Form.Label>Message component here &#9993;</Form.Label>
+                      <Form.Control
+                        as="textarea"
+                        rows={3}
+                        // type="text"
+                        // name="username"
+                        // placeholder="Enter username"
+                        // value={username}
+                        // onChange={handleChange}
+                      />
+                    </Form.Group>
+                    <Button className="button" variant="primary" type="submit">
+                      Send message
+                    </Button>
+                  </Form>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
         )}
       </div>
-      <Card>
-        <Card.Body>
-          <div>Message component here</div>
-        </Card.Body>
-      </Card>
     </>
   );
 }
